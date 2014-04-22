c4d23fb4303c99c030212924d7f61715a39c31ca
Boxmuncher <tharwood1@gmail.com>
2014-04-22 12:23:29 -0400

    first commit

---
+++ Assets/Scripts/Transparent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transparent : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject glass = GameObject.Find("Glass");
+		Color color = renderer.material.color;
+		color.a = 0.5f;
+		glass.renderer.material.color = color;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
